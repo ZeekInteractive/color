9f391f6b4264e88c3284a44a6a13bd880ba5e62a
Jesse Kramer <kram0021@hz.nl>
2017-06-18 23:00:36 +0200

    improved test quality

--- tests/ColorTest.php
+++ tests/ColorTest.php
@@ -51,6 +51,9 @@ final class ColorTest extends TestCase
         $rgb = Color::fromRGB(123, 123, 123)->toRGB();
         $this->assertEquals([123, 123, 123], $rgb);
 
+        $rgb = Color::fromRGB(200, 123, 123)->toRGB();
+        $this->assertEquals([200, 123, 123], $rgb);
+
         $rgbString = Color::fromRGB(123, 123, 123)->toRGBString();
         $this->assertEquals('rgb(123, 123, 123)', $rgbString);
     }
@@ -59,6 +62,9 @@ final class ColorTest extends TestCase
     {
         $hex = Color::fromHEX('#1E90FF')->toHEX();
         $this->assertEquals('#1E90FF', $hex);
+
+        $hex = Color::fromHEX('#C44')->toHEX();
+        $this->assertEquals('#CC4444', $hex);
 
         $hexString = Color::fromHEX('#1E90FF')->toHEXString();
         $this->assertEquals('#1E90FF', $hexString);
@@ -78,6 +84,12 @@ final class ColorTest extends TestCase
         $hsv = Color::fromHSV(200, 80, 40)->toHSV();
         $this->assertEquals([200, 80, 40], $hsv);
 
+        $hsv = Color::fromHSV(250, 80, 40)->toHSV();
+        $this->assertEquals([250, 80, 40], $hsv);
+
+        $hsv = Color::fromHSV(310, 80, 40)->toHSV();
+        $this->assertEquals([310, 80, 40], $hsv);
+
         $hsvString = Color::fromHSV(200, 80, 40)->toHSVString();
         $this->assertEquals('hsv(200, 80, 40)', $hsvString);
     }
@@ -86,23 +98,35 @@ final class ColorTest extends TestCase
     {
         $darkColor = Color::fromHEX('#1E90FF')->darken(10)->toHEX();
         $this->assertEquals('#0182FF', $darkColor);
+
+        $this->expectException(InvalidArgumentException::class);
+        $darkColor = Color::fromHEX('#1E90FF')->darken(110)->toHEX();
     }
 
     public function testColorAlterationLighten()
     {
         $lightColor = Color::fromHEX('#1E90FF')->lighten(10)->toHEX();
         $this->assertEquals('#359BFF', $lightColor);
+
+        $this->expectException(InvalidArgumentException::class);
+        $lightColor = Color::fromHEX('#1E90FF')->lighten(110)->toHEX();
     }
 
     public function testColorAlterationSaturate()
     {
         $exitingColor = Color::fromHEX('#1E90FF')->saturate(10)->toHEX();
         $this->assertEquals('#1E90FF', $exitingColor);
+
+        $this->expectException(InvalidArgumentException::class);
+        $exitingColor = Color::fromHEX('#1E90FF')->saturate(110)->toHEX();
     }
 
     public function testColorAlterationDesaturate()
     {
         $dullColor = Color::fromHEX('#1E90FF')->desaturate(10)->toHEX();
         $this->assertEquals('#2990F4', $dullColor);
+
+        $this->expectException(InvalidArgumentException::class);
+        $dullColor = Color::fromHEX('#1E90FF')->desaturate(110)->toHEX();
     }
 }
